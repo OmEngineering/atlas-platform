d7ae311c3438c8adf0acf9d6bb353ac001f2f694
Om Lathiya <lathiya52725@gmail.com>
2026-07-11 17:23:48 +0530

    feat: initialize spring boot project

---
+++ src/main/java/com/atlas/AtlasPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.atlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtlasPlatformApplication.class, args);
+        System.out.println("Platform is running......");
+    }
+}
